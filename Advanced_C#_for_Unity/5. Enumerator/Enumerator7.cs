39edfd9fd3e58f45ced098ad493880f3e9985ee3
유저이름 <qquqqu26@naver.com>
2025-03-23 17:04:54 +0900

    이뉴머레이터8

---
+++ Advanced_C#_for_Unity/5. Enumerator/Enumerator7.cs
@@ -0,0 +1,49 @@
+/*
+ * 무한 루프에서 벗어나기 위해 코드를 완전히 작성함 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enumerator7 : MonoBehaviour
+{
+
+    public class MyList
+    {
+        public enumerator GetEnumerator()
+        {
+            enumerator enumerator = new enumerator();
+            return enumerator; 
+        }
+
+        public class enumerator
+        {
+            //보충한 부분
+            int[] num = new int[5] {1, 2, 3, 4, 5}; //순회할 배열
+            int index = -1;             //순회시킬 인덱스
+
+            //Current 속성: 현재 index가 참조하는 배열값 반환
+            public object Current { get { return num[index]; } }
+
+            //MoveNext() 메서드: 
+            //index 다음 배열값에 접근 할 수 있는 지
+            public bool MoveNext()
+            {
+                if (index == num.Length-1) return false;
+
+                //접근 가능하면 index를 다음 배열가리키게 
+                index++; 
+                return index < num.Length;
+            }
+        }
+    }
+    void Start()
+    {
+        MyList myList = new MyList();
+        foreach (var item in myList) 
+        {
+            Debug.Log(item);
+        }
+    }
+
+}
--- Advanced_C#_for_Unity/5. Enumerator/Enumerator8.cs
+++ Advanced_C#_for_Unity/5. Enumerator/Enumerator8.cs
@@ -1,12 +1,11 @@
 /*
  * 무한 루프에서 벗어나기 위해 코드를 완전히 작성함 
  */
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class Enumerator8 : MonoBehaviour
 {
 
     public class MyList
@@ -14,13 +13,13 @@ public class test : MonoBehaviour
         public enumerator GetEnumerator()
         {
             enumerator enumerator = new enumerator();
-            return enumerator; 
+            return enumerator;
         }
 
         public class enumerator
         {
             //보충한 부분
-            int[] num = new int[5] {1, 2, 3, 4, 5}; //순회할 배열
+            int[] num = new int[5] { 1, 2, 3, 4, 5 }; //순회할 배열
             int index = -1;             //순회시킬 인덱스
 
             //Current 속성: 현재 index가 참조하는 배열값 반환
@@ -30,21 +29,28 @@ public class test : MonoBehaviour
             //index 다음 배열값에 접근 할 수 있는 지
             public bool MoveNext()
             {
-                if (index == num.Length-1) return false;
+                if (index == num.Length - 1) return false;
 
                 //접근 가능하면 index를 다음 배열가리키게 
-                index++; 
+                index++;
                 return index < num.Length;
             }
         }
     }
+
     void Start()
     {
         MyList myList = new MyList();
-        foreach (var item in myList) 
-        {
-            Debug.Log(item);
-        }
+        MyList.enumerator e = myList.GetEnumerator();
+
+        e.MoveNext();
+        print($"{e.Current}");
+        e.MoveNext();
+        print($"{e.Current}");
+        e.MoveNext();
+        print($"{e.Current}");
+        e.MoveNext();
+        print($"{e.Current}");
     }
 
 }
